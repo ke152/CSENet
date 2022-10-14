806aec0d0071cbbb0f3d3ae3eadfca65f8624bfb
ke152 <ke152@foxmail.com>
2022-10-15 00:37:26 +0800

    Create CSENetChannel.cs

---
+++ CSENet/CSENetChannel.cs
@@ -0,0 +1,18 @@
+﻿namespace CSENet;
+
+class CSENetChannel
+{
+    public uint outReliableSeqNum = 0;
+    public uint outUnreliableSeqNum = 0;
+    public int usedReliableWindows = 0;
+    public uint[] reliableWindows = new uint[(int)CSENetDef.PeerReliableWindows];
+    public uint inReliableSeqNum = 0;
+    public uint inUnreliableSeqNum = 0;
+    public List<CSENetInCmd> inReliableCmds = new();
+    public List<CSENetInCmd> inUnreliableCmds = new();
+
+    public CSENetChannel()
+    {
+
+    }
+}
