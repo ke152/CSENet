9b0a4eee2ac66a483a9a912be51988e281f42501
ke152 <ke152@foxmail.com>
2022-10-15 00:37:48 +0800

    Create CSENetCmd.cs

---
+++ CSENet/CSENetCmd.cs
@@ -0,0 +1,33 @@
+﻿namespace CSENet;
+
+class CSENetAckCmd
+{
+    public uint sentTime;
+    public CSENetProtoCmdHeader cmdHeader = new();
+};
+
+class CSENetOutCmd
+{
+    public uint reliableSeqNum;
+    public uint unreliableSeqNum;
+    public long sentTime;
+    public long rttTimeout;
+    public long rttTimeoutLimit;
+    public uint fragmentOffset;
+    public uint fragmentLength;
+    public uint sendAttempts;
+    public CSENetProtoCmdHeader cmdHeader = new();
+    public CSENetProto cmd = new();
+    public CSENetPacket? packet;
+};
+
+class CSENetInCmd
+{
+    public uint reliableSeqNum;
+    public uint unreliableSeqNum;
+    public CSENetProtoCmdHeader cmdHeader = new();
+    public uint fragmentCount;
+    public uint fragmentsRemaining;
+    public uint[]? fragments;
+    public CSENetPacket? packet = null;
+};
