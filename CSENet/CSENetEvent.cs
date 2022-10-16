25d62e0f94f9363577512751e24f786653c6edc1
ke152 <ke152@foxmail.com>
2022-10-16 11:32:35 +0800

    Create CSENetEvent.cs

---
+++ CSENet/CSENetEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace CSENet;
+
+enum CSENetEventType
+{
+    None = 0,
+    Connect = 1,
+    Disconnect = 2,
+    Recv = 3
+}
+
+internal class CSENetEvent
+{
+        public CSENetEventType type = CSENetEventType.None;      /**< type of the event */
+        public CSENetPeer? peer = null;      /**< peer that generated a connect, disconnect or receive event */
+        public uint channelID; /**< channel on the peer that generated the event, if appropriate */
+        public uint data;      /**< data associated with the event, if appropriate */
+        public CSENetPacket? packet = null;    /**< packet associated with the event, if appropriate */
+
+        public CSENetEvent()
+        {
+
+        }
+
+}
