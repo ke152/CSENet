ecca80b3608dbf737e31ae5940ada62d3ad6e134
ke152 <ke152@foxmail.com>
2022-10-16 11:32:43 +0800

    Create CSENetHost.cs

---
+++ CSENet/CSENetHost.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+
+namespace CSENet;
+
+internal class CSENetHost
+{
+    public CSENetSocket? socket;
+    public IPEndPoint? address;                     /**< Internet address of the host */
+    public uint inBandwidth;           /**< downstream bandwidth of the host */
+    public uint outBandwidth;           /**< upstream bandwidth of the host */
+    public uint bandwidthThrottleEpoch;
+    public uint mtu;
+    public uint randomSeed;
+    public int recalculateBandwidthLimits;
+    public CSENetPeer[]? peers;                       /**< array of peers allocated for this host */
+    public uint peerCount;                   /**< number of peers allocated for this host */
+    public uint channelLimit;                /**< maximum number of channels allowed for connected peers */
+    public long serviceTime;
+    public List<CSENetPeer> dispatchQueue = new();
+    public int continueSending;
+    public uint packetSize;
+    public uint headerFlags;
+    public List<CSENetProto> commands = new();// 不能超过：ENetDef.ProtoMaxPacketCmds
+    public int commandCount { get { return this.commands.Count; } }
+    public List<byte[]> buffers = new List<byte[]>();//不能超过： ENetDef.BufferMax
+    public int bufferCount { get { return this.buffers.Count; } }
+    public byte[][] packetData;
+    public IPEndPoint? receivedAddress;
+    public byte[]? receivedData;
+    public int receivedDataLength;
+    public uint totalSentData;               /**< total data sent, user should reset to 0 as needed to prevent overflow */
+    public uint totalSentPackets;            /**< total UDP packets sent, user should reset to 0 as needed to prevent overflow */
+    public int totalReceivedData;           /**< total data received, user should reset to 0 as needed to prevent overflow */
+    public uint totalReceivedPackets;        /**< total UDP packets received, user should reset to 0 as needed to prevent overflow */
+    public uint connectedPeers;
+    public uint bandwidthLimitedPeers;
+    public uint duplicatePeers;              /**< optional number of allowed peers from duplicate IPs, defaults to Proto_MAXIMUMPEERID */
+    public uint maximumPacketSize;           /**< the maximum allowable packet size that may be sent or received on a peer */
+    public uint maximumWaitingData;          /**< the maximum aggregate amount of buffer space a peer may use waiting for packets to be delivered */
+
+}
