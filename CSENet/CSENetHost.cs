d2027f8d97feda72d20dd562c2b6c64363693cb4
ke152 <ke152@foxmail.com>
2022-10-17 21:58:03 +0800

    update: init function

--- CSENet/CSENetHost.cs
+++ CSENet/CSENetHost.cs
@@ -21,10 +21,10 @@ internal class CSENetHost
     public uint packetSize;
     public uint headerFlags;
     public List<CSENetProto> commands = new();// 不能超过：ENetDef.ProtoMaxPacketCmds
-    public int commandCount { get { return this.commands.Count; } }
-    public List<byte[]> buffers = new List<byte[]>();//不能超过： ENetDef.BufferMax
-    public int bufferCount { get { return this.buffers.Count; } }
-    public byte[][] packetData;
+    public int CommandCount { get { return this.commands.Count; } }
+    public List<byte[]> buffers = new();//不能超过： ENetDef.BufferMax
+    public int BufferCount { get { return this.buffers.Count; } }
+    public byte[]?[]? packetData;
     public IPEndPoint? receivedAddress;
     public byte[]? receivedData;
     public int receivedDataLength;
@@ -38,4 +38,87 @@ internal class CSENetHost
     public uint maximumPacketSize;           /**< the maximum allowable packet size that may be sent or received on a peer */
     public uint maximumWaitingData;          /**< the maximum aggregate amount of buffer space a peer may use waiting for packets to be delivered */
 
+    public CSENetHost()
+    {
+        this.packetData = new byte[2][];
+        this.packetData[0] = new byte[CSENetDef.ProtoMaxMTU];
+        this.packetData[1] = new byte[CSENetDef.ProtoMaxMTU];
+    }
+
+    public void Create(IPEndPoint? address, uint peerCount, uint channelLimit, uint incomingBandwidth, uint outgoingBandwidth)
+    {
+        if (peerCount > CSENetDef.ProtoMaxPeerID)
+            return;
+
+        this.peers = new CSENetPeer[peerCount];
+        for (int i = 0; i < peerCount; i++)
+        {
+            this.peers[i] = new CSENetPeer();
+        }
+
+        this.socket = new CSENetSocket();
+
+        if (address != null)
+        {
+            this.socket.Bind(address);
+        }
+
+        this.socket.SetOption(CSENetSocketOptType.NonBlock, 1);
+        this.socket.SetOption(CSENetSocketOptType.Broadcast, 1);
+        this.socket.SetOption(CSENetSocketOptType.RcvBuf, (int)CSENetDef.HostRecvBufferSize);
+        this.socket.SetOption(CSENetSocketOptType.SendBuf, (int)CSENetDef.HostSendBufferSize);
+        this.socket.SetOption(CSENetSocketOptType.RcvTimeout, 1000);
+        this.socket.SetOption(CSENetSocketOptType.SendTimeout, 1000);
+
+        this.address = address;
+
+        if (channelLimit != 0 || channelLimit > CSENetDef.ProtoMaxChannelCount)
+            channelLimit = CSENetDef.ProtoMaxChannelCount;
+        else
+        if (channelLimit < CSENetDef.ProtoMinChannelCount)
+            channelLimit = CSENetDef.ProtoMinChannelCount;
+
+        this.randomSeed = (uint)(new Random()).Next(0);
+        this.randomSeed += CSENetUtils.RandomSeed();
+        this.randomSeed = (this.randomSeed << 16) | (this.randomSeed >> 16);
+        this.channelLimit = channelLimit;
+        this.inBandwidth = incomingBandwidth;
+        this.outBandwidth = outgoingBandwidth;
+        this.bandwidthThrottleEpoch = 0;
+        this.recalculateBandwidthLimits = 0;
+        this.mtu = CSENetDef.HostDefaultMTU;
+        this.peerCount = peerCount;
+        this.receivedAddress = new IPEndPoint(IPAddress.Any, 0);
+        this.receivedData = null;
+        this.receivedDataLength = 0;
+
+        this.totalSentData = 0;
+        this.totalSentPackets = 0;
+        this.totalReceivedData = 0;
+        this.totalReceivedPackets = 0;
+
+        this.connectedPeers = 0;
+        this.bandwidthLimitedPeers = 0;
+        this.duplicatePeers = CSENetDef.ProtoMaxPeerID;
+        this.maximumPacketSize = CSENetDef.HostDefaultMaxPacketSize;
+        this.maximumWaitingData = CSENetDef.HostDefaultMaxWaintingData;
+
+        this.dispatchQueue.Clear();
+
+        for (uint i = 0; i < this.peers.Length; i++)
+        {
+            var currentPeer = this.peers[i];
+            currentPeer.inPeerID = i;
+            currentPeer.outSessionID = currentPeer.inSessionID = 0xFF;
+            currentPeer.data = null;
+
+            currentPeer.acknowledgements.Clear();
+            currentPeer.sentReliableCmds.Clear();
+            currentPeer.sentUnreliableCmds.Clear();
+            currentPeer.outCmds.Clear();
+            currentPeer.dispatchedCmds.Clear();
+
+            currentPeer.Reset();
+        }
+    }
 }
--- CSENet/CSENetPeer.cs
+++ CSENet/CSENetPeer.cs
@@ -28,7 +28,7 @@ internal class CSENetPeer
     public byte[]? data;               /**< Application private data, may be freely modified */
     public CSENetPeerState state;
     public CSENetChannel[]? channels;
-    public int channelCount
+    public int ChannelCount
     {
         get
         {
@@ -82,4 +82,94 @@ internal class CSENetPeer
     public uint[] unseqWindow = new uint[CSENetDef.PeerUnseqWindowSize / 32];
     public uint eventData;
     public uint totalWaitingData;
+
+    public void Reset()
+    {
+        OnDisconnect();
+
+        this.outPeerID = CSENetDef.ProtoMaxPeerID;
+        this.connectID = 0;
+
+        this.state = CSENetPeerState.Disconnected;
+
+        this.inBandwidth = 0;
+        this.outBandwidth = 0;
+        this.inBandwidthThrottleEpoch = 0;
+        this.outBandwidthThrottleEpoch = 0;
+        this.inDataTotal = 0;
+        this.outDataTotal = 0;
+        this.lastSendTime = 0;
+        this.lastReceiveTime = 0;
+        this.nextTimeout = 0;
+        this.earliestTimeout = 0;
+        this.packetLossEpoch = 0;
+        this.packetsSent = 0;
+        this.packetsLost = 0;
+        this.packetLoss = 0;
+        this.packetLossVariance = 0;
+        this.packetThrottle = CSENetDef.PeerDefaultPacketThrottle;
+        this.packetThrottleLimit = CSENetDef.PeerPacketThrottleScale;
+        this.packetThrottleCounter = 0;
+        this.packetThrottleEpoch = 0;
+        this.packetThrottleAcceleration = CSENetDef.PeerPacketThrottleAcceleration;
+        this.packetThrottleDeceleration = CSENetDef.PeerPacketThrottleDeceleration;
+        this.packetThrottleInterval = CSENetDef.PeerPacketThrottleInterval;
+        this.pingInterval = CSENetDef.PeerPingInterval;
+        this.timeoutLimit = CSENetDef.PeerTimeoutLimit;
+        this.timeoutMinimum = CSENetDef.PeerTimeoutMin;
+        this.timeoutMaximum = CSENetDef.PeerTimeoutMax;
+        this.lastRoundTripTime = CSENetDef.PeerDefaultRTT;
+        this.lowestRoundTripTime = CSENetDef.PeerDefaultRTT;
+        this.lastRTTVariance = 0;
+        this.highestRoundTripTimeVariance = 0;
+        this.rtt = CSENetDef.PeerDefaultRTT;
+        this.rttVariance = 0;
+        this.mtu = this.host == null ? 0 : this.host.mtu;
+        this.reliableDataInTransit = 0;
+        this.outReliableSeqNum = 0;
+        this.windowSize = CSENetDef.ProtoMaxWindowSize;
+        this.inUnseqGroup = 0;
+        this.outUnSeqGroup = 0;
+        this.eventData = 0;
+        this.totalWaitingData = 0;
+        this.needDispatch = false;
+
+        Array.Clear(this.unseqWindow);
+
+        ResetQueues();
+    }
+
+    public void OnDisconnect()
+    {
+        if (this.host == null)
+            return;
+
+        if (state == CSENetPeerState.Connected || state == CSENetPeerState.DisconnectLater)
+        {
+            if (inBandwidth != 0)
+            {
+                this.host.bandwidthLimitedPeers--;
+            }
+
+            this.host.connectedPeers--;
+        }
+    }
+
+    public void ResetQueues()
+    {
+        if (this.host != null)
+        {
+            this.host.dispatchQueue.Remove(this);
+        }
+
+        needDispatch = false;
+
+        acknowledgements.Clear();
+        sentReliableCmds.Clear();
+        sentUnreliableCmds.Clear();
+        outCmds.Clear();
+        dispatchedCmds.Clear();
+
+        channels = null;
+    }
 }
