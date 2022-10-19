100a225c00181ec783f58d7652fdb2da08ba1782
ke152 <ke152@foxmail.com>
2022-10-19 20:10:01 +0800

    update: BandwidthThrottle()

--- CSENet/CSENetHost.cs
+++ CSENet/CSENetHost.cs
@@ -164,6 +164,173 @@ public class CSENetHost
     }
 
 
+    public void BandwidthThrottle()
+    {
+        uint timeCurrent = (uint)CSENetUtils.TimeGet();
+        uint elapsedTime = timeCurrent - this.bandwidthThrottleEpoch,
+           peersRemaining = (uint)this.connectedPeers,
+           bandwidth = uint.MaxValue,
+           throttle,
+           bandwidthLimit = 0;
+        int dataTotal = int.MaxValue;
+        int needsAdjustment = this.bandwidthLimitedPeers > 0 ? 1 : 0;
+
+        if (elapsedTime < CSENetDef.HostBandwidthThrottleInterval)
+            return;
+
+        this.bandwidthThrottleEpoch = timeCurrent;
+
+        if (peersRemaining == 0 || this.peers == null)
+            return;
+
+        if (this.outBandwidth != 0)
+        {
+            dataTotal = 0;
+            bandwidth = (this.outBandwidth * elapsedTime) / 1000;
+
+            if (this.peers != null)
+            {
+                foreach (var peer in this.peers)
+                {
+                    if (peer.state != CSENetPeerState.Connected && peer.state != CSENetPeerState.DisconnectLater)
+                        continue;
+
+                    dataTotal += peer.outDataTotal;
+                }
+            }
+        }
+
+        while (peersRemaining > 0 && needsAdjustment != 0)
+        {
+            needsAdjustment = 0;
+
+            if (dataTotal <= bandwidth)
+                throttle = CSENetDef.PeerPacketThrottleScale;
+            else
+                throttle = (bandwidth * CSENetDef.PeerPacketThrottleScale) / (uint)dataTotal;
+
+            if (this.peers != null)
+            {
+                foreach (var peer in this.peers)
+                {
+                    uint peerBandwidth;
+
+                    if ((peer.state != CSENetPeerState.Connected && peer.state != CSENetPeerState.DisconnectLater) ||
+                        peer.inBandwidth == 0 ||
+                        peer.outBandwidthThrottleEpoch == timeCurrent)
+                        continue;
+
+                    peerBandwidth = (peer.inBandwidth * elapsedTime) / 1000;
+                    if ((throttle * peer.outDataTotal) / CSENetDef.PeerPacketThrottleScale <= peerBandwidth)
+                        continue;
+
+                    peer.packetThrottleLimit = (peerBandwidth *
+                                                    CSENetDef.PeerPacketThrottleScale) / (uint)peer.outDataTotal;
+
+                    if (peer.packetThrottleLimit == 0)
+                        peer.packetThrottleLimit = 1;
+
+                    if (peer.packetThrottle > peer.packetThrottleLimit)
+                        peer.packetThrottle = peer.packetThrottleLimit;
+
+                    peer.outBandwidthThrottleEpoch = timeCurrent;
+
+                    peer.inDataTotal = 0;
+                    peer.outDataTotal = 0;
+
+                    needsAdjustment = 1;
+                    --peersRemaining;
+                    bandwidth -= peerBandwidth;
+                    dataTotal -= (int)peerBandwidth;
+                }
+            }
+        }
+
+        if (peersRemaining > 0)
+        {
+            if (dataTotal <= bandwidth)
+                throttle = CSENetDef.PeerPacketThrottleScale;
+            else
+                throttle = (bandwidth * CSENetDef.PeerPacketThrottleScale) / (uint)dataTotal;
+
+            if (this.peers != null)
+            {
+                foreach (var peer in this.peers)
+                {
+                    if ((peer.state != CSENetPeerState.Connected && peer.state != CSENetPeerState.DisconnectLater) ||
+                        peer.outBandwidthThrottleEpoch == timeCurrent)
+                        continue;
+
+                    peer.packetThrottleLimit = throttle;
+
+                    if (peer.packetThrottle > peer.packetThrottleLimit)
+                        peer.packetThrottle = peer.packetThrottleLimit;
+
+                    peer.inDataTotal = 0;
+                    peer.outDataTotal = 0;
+                }
+            }
+        }
+
+        if (this.recalculateBandwidthLimits != 0)
+        {
+            this.recalculateBandwidthLimits = 0;
+
+            peersRemaining = (uint)this.connectedPeers;
+            bandwidth = this.inBandwidth;
+            needsAdjustment = 1;
+
+            if (bandwidth == 0)
+                bandwidthLimit = 0;
+            else
+                while (peersRemaining > 0 && needsAdjustment != 0)
+                {
+                    needsAdjustment = 0;
+                    bandwidthLimit = bandwidth / peersRemaining;
+
+                    if (this.peers == null) break;
+
+                    foreach (var peer in this.peers)
+                    {
+                        if ((peer.state != CSENetPeerState.Connected && peer.state != CSENetPeerState.DisconnectLater) ||
+                            peer.inBandwidthThrottleEpoch == timeCurrent)
+                            continue;
+
+                        if (peer.outBandwidth > 0 &&
+                            peer.outBandwidth >= bandwidthLimit)
+                            continue;
+
+                        peer.inBandwidthThrottleEpoch = timeCurrent;
+
+                        needsAdjustment = 1;
+                        --peersRemaining;
+                        bandwidth -= peer.outBandwidth;
+                    }
+                }
+
+            if (this.peers != null)
+            {
+                foreach (var peer in this.peers)
+                {
+                    if (peer.state != CSENetPeerState.Connected && peer.state != CSENetPeerState.DisconnectLater)
+                        continue;
+
+                    CSENetProto command = new();
+                    command.header.cmdFlag = (int)CSENetProtoCmdType.BandwidthLimit | (int)CSENetProtoFlag.CmdFlagAck;
+                    command.header.channelID = 0xFF;
+                    command.bandwidthLimit = new();
+                    command.bandwidthLimit.outBandwidth = (uint)IPAddress.HostToNetworkOrder(this.outBandwidth);
+
+                    if (peer.inBandwidthThrottleEpoch == timeCurrent)
+                        command.bandwidthLimit.inBandwidth = (uint)IPAddress.HostToNetworkOrder(peer.outBandwidth);
+                    else
+                        command.bandwidthLimit.inBandwidth = (uint)IPAddress.HostToNetworkOrder(bandwidthLimit);
+
+                    peer.QueueOutgoingCommand(command, null, 0, 0);
+                }
+            }
+        }
+    }
 
 
 
